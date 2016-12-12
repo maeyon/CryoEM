47e4eb3b0599a7403234c4167aa3caf7a2ecf82c
KenjiTAKIGUCHI <takiguchi.kenji.35z@st.kyoto-u.ac.jp>
2016-12-12 18:58:28 +0900

    Estimation of Initial Value of 'L'
    使わないプログラムを削除し、Lの初期値

---
+++ linerfunc.m
@@ -0,0 +1,4 @@
+fplot(@(x) 1,[0 1],'r')
+fplot(@(x) -x+2,[1 2],'r')
+fplot(@(x) 0,[2 3],'r')
+hold on
---
+++ spline.m
@@ -0,0 +1,9 @@
+%ƒXƒvƒ‰ƒCƒ“•âŠÔ
+% given: x_n, y_n
+% def
+
+h_j = x_{j+1} - x_j
+w_j = y_{j+1} - y_j
+v_j = 6 * (w_j/h_j -w_{j-1}/h_{j-1}
+
+
