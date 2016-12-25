564360ad2dc35a81c5850deb78c3054b3b2cc651
KenjiTAKIGUCHI <takiguchi.kenji.35z@st.kyoto-u.ac.jp>
2016-12-26 02:42:34 +0900

    myspline_estを修正しました

--- myspline_est.m
+++ myspline_est.m
@@ -3,7 +3,9 @@
 function s = myspline_est(tc,tr)
 % ’è‹`
 h = tc(2:end)-tc(1:end-1);
-v = 6*((tr(3:end)-tr(2:end-1))./h(2:end) + tr(2:end-1)-tr(1:end-2)./h(1:end-1));
+v_1 = tr(3:end)-tr(2:end-1)./h(2:end);
+v_2 = tr(2:end-1)-tr(1:end-2)./h(1:end-1);
+v = 6*(v_1 - v_2);
 
 %s—ñH‚Ì¶¬
 diagH_0 = 2*(h(1:end-1) + h(2:end));
@@ -15,9 +17,9 @@ H_2 = [zeros(1,N) ; diag(diagH_1), zeros(N-1,1)];
 H = H_0 + H_1 + H_2;
 
 %u, a,b,c,d, ‹æ•ª‘½€®, ‹Èü‚ğ‹‚ß‚é
-w = H\v;
+w = H\v(:);
 u = [0 ; w(:) ; 0];
-a = u(2:end) - u(1:end-1) ./ (6*h);
+a = (u(2:end) - u(1:end-1)) ./ (6*h);
 b = u(1:end-1) / 2;
 c = (tr(2:end) - tr(1:end-1)) ./ h(1:end) - h(1:end).*(2*u(1:end-1)+u(2:end))/6;
 d = tr(1:end-1);
